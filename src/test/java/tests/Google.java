e175222e3a5473f0641f623dbe28b7941e3be4cf
beckkari8 <karimovbektur@gmail.com>
2023-09-30 18:37:47 -0500

    new changes

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -13,6 +13,8 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 import utilities.Driver;
 
+import java.sql.SQLOutput;
+
 public class Google {
 
 
@@ -20,9 +22,14 @@ public class Google {
     public void seach () {
         Faker faker = new Faker();
         String testData = faker.lorem().word();
+        Driver.setDriver("chrome").get("https://google.com");
         Driver.setDriver("chrome").findElement(By.name("q")).sendKeys(testData + Keys.ENTER);
         String title = Driver.setDriver("chrome").getTitle();
         Assert.assertTrue("it is failed", title.contains(testData));
-
     }
+
+    public void googleImage (){
+        Driver.setDriver("chrome").get("https://google.com");
+        System.out.println("checking some images");
+            }
 }
